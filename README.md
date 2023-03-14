bab2e50a64835e01a4ac2e5eec1cb183d9066bbe
Zacc Elisha <zacheause@gmail.com>
2023-03-14 17:08:46 +0100

    Fixed the recaptcha form

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ Go into ModalDialog.js file and make changes for the email services to run.
 
 # Phone Number
 
-Comments were placed in the files where the phone numbers needs to be updated.s
+Comments were placed in the files where the phone numbers needs to be updated.
+
+# Recaptcha
+
+To test the recaptcha on the Contact Us page, start the server in terminal with the syntax:
+
+```
+npm run server
+```
+
+Also, go to the [Google Recaptcha Website](https://www.google.com/recaptcha/admin/create) to create your own site key and secret key.
+
+Use .env to store the values as `REACT_APP_SITE_KEY` and `REACT_APP_SECRET_KEY`.
+
+Also, change the port to your own backend port at `ContactForm.js`.
--- acs/src/components/ContactForm.js
+++ acs/src/components/ContactForm.js
@@ -9,6 +9,7 @@ const ContactForm = () => {
 	const [recaptchaToken, setRecaptchaToken] = useState(null)
 	const [invalidCount, setInvalidCount] = useState(0)
 	const [validated, setValidated] = useState(false)
+	const [success, setSuccess] = useState(false)
 	const [name, setName] = useState("")
 	const [email, setEmail] = useState("")
 	const [num, setNum] = useState("")
@@ -39,96 +40,103 @@ const ContactForm = () => {
 		console.log(token)
 
 		console.log(process.env)
-
+		// Change the node port from 2000 to your backend port
 		await axios
-			.post(process.env.REACT_APP_API_URL, { token })
+			.post(`http://localhost:2000/post`, { token })
 			.then((res) => console.log(res))
 			.catch((error) => {
 				console.log(error)
 			})
+		setSuccess(true)
 	}
 
 	return (
-		<Form
-			noValidate
-			validated={validated}
-			onSubmit={handleSubmit}>
-			<h1>Contact Us</h1>
-			<Form.Group className={validated && !nameRef.current.value ? "invalid mb-3" : "mb-3"}>
-				<Form.Control
-					type="text"
-					placeholder="Name"
-					pattern="[A-Za-z]+"
-					ref={nameRef}
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					required
-				/>
-				<Form.Control.Feedback
-					key={`name-feedback-${invalidCount}`}
-					type="invalid">
-					Please enter your name.
-				</Form.Control.Feedback>
-			</Form.Group>
-			<Form.Group className={validated && !emailRef.current.value ? "invalid mb-3" : "mb-3"}>
-				<Form.Control
-					type="email"
-					placeholder="Email"
-					ref={emailRef}
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					required
-				/>
-				<Form.Control.Feedback
-					key={`email-feedback-${invalidCount}`}
-					type="invalid">
-					Please enter a valid email address.
-				</Form.Control.Feedback>
-			</Form.Group>
-			<Form.Group className={validated && !numRef.current.value ? "invalid mb-3" : "mb-3"}>
-				<Form.Control
-					type="tel"
-					placeholder="Phone Number"
-					pattern="[A-Za-z0-9-]+"
-					ref={numRef}
-					value={num}
-					onChange={(e) => setNum(e.target.value)}
-					required
-				/>
-				<Form.Control.Feedback
-					key={`number-feedback-${invalidCount}`}
-					type="invalid">
-					Please enter a valid phone number.
-				</Form.Control.Feedback>
-			</Form.Group>
-			<Form.Group className={validated && !messageRef.current.value ? "invalid mb-3" : "mb-3"}>
-				<Form.Control
-					as="textarea"
-					rows={3}
-					placeholder="Message"
-					ref={messageRef}
-					value={message}
-					onChange={(e) => setMessage(e.target.value)}
-					required
-				/>
-			</Form.Group>
-			<div className="recaptcha mb-3">
-				<ReCAPTCHA
-					sitekey={process.env.REACT_APP_SITE_KEY}
-					ref={recaptchaRef}
-					onChange={handleRecaptcha}
-					data-badge="inline"
-				/>
-			</div>
-			<div className="form-btn">
-				<Button
-					variant="dark"
-					type="submit"
-					size="lg">
-					Send
-				</Button>
-			</div>
-		</Form>
+		<>
+			{!success ? (
+				<Form
+					noValidate
+					validated={validated}
+					onSubmit={handleSubmit}>
+					<h1>Contact Us</h1>
+					<Form.Group className={validated && !nameRef.current.value ? "invalid mb-3" : "mb-3"}>
+						<Form.Control
+							type="text"
+							placeholder="Name"
+							pattern="[A-Za-z]+"
+							ref={nameRef}
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							required
+						/>
+						<Form.Control.Feedback
+							key={`name-feedback-${invalidCount}`}
+							type="invalid">
+							Please enter your name.
+						</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group className={validated && !emailRef.current.value ? "invalid mb-3" : "mb-3"}>
+						<Form.Control
+							type="email"
+							placeholder="Email"
+							ref={emailRef}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+						<Form.Control.Feedback
+							key={`email-feedback-${invalidCount}`}
+							type="invalid">
+							Please enter a valid email address.
+						</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group className={validated && !numRef.current.value ? "invalid mb-3" : "mb-3"}>
+						<Form.Control
+							type="tel"
+							placeholder="Phone Number"
+							pattern="[A-Za-z0-9-]+"
+							ref={numRef}
+							value={num}
+							onChange={(e) => setNum(e.target.value)}
+							required
+						/>
+						<Form.Control.Feedback
+							key={`number-feedback-${invalidCount}`}
+							type="invalid">
+							Please enter a valid phone number.
+						</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group className={validated && !messageRef.current.value ? "invalid mb-3" : "mb-3"}>
+						<Form.Control
+							as="textarea"
+							rows={3}
+							placeholder="Message"
+							ref={messageRef}
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+							required
+						/>
+					</Form.Group>
+					<div className="recaptcha mb-3">
+						<ReCAPTCHA
+							sitekey={process.env.REACT_APP_SITE_KEY}
+							ref={recaptchaRef}
+							onChange={handleRecaptcha}
+							data-badge="inline"
+						/>
+					</div>
+					<div className="form-btn">
+						<Button
+							variant="dark"
+							type="submit"
+							size="lg">
+							Send
+						</Button>
+					</div>
+				</Form>
+			) : (
+				<h2 style={{ textAlign: "center", marginTop: "40px" }}>Thank you for contacting us!</h2>
+			)}
+		</>
 	)
 }
 
